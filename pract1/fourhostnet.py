9ba6933d6a83f434e5d90e59d8cf767087d0aa85
alejandalb98 <58817715+alejandalb98@users.noreply.github.com>
2021-12-22 01:58:02 +0100

    Subida práctica 1

---
+++ pract1/fourhostnet.py
@@ -0,0 +1,47 @@
+from mininet.net import Mininet
+from mininet.node import OVSController
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+
+def emptyNet():
+
+    "Create an empty network and add nodes to it."
+
+    net = Mininet( controller=OVSController )
+
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.0.1' )
+    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+    h3 = net.addHost( 'h3', ip='10.0.0.3' )
+    h4 = net.addHost( 'h4', ip='10.0.0.4' )
+
+    info("MAC de los hosts\n")
+    info("MAC h1: "+h1.MAC()+"\n")
+    info("MAC h2: "+h2.MAC()+"\n")
+    info("MAC h3: "+h3.MAC()+"\n")
+    info("MAC h4: "+h4.MAC()+"\n")
+
+    info( '*** Adding switch\n' )
+    s1 = net.addSwitch( 's1' )
+
+    info( '*** Creating links\n' )
+    net.addLink( h1, s1, bw=10, delay='10ms', max_queue_size=1000, loss=10, use_htb=True)
+    net.addLink( h2, s1, bw=10, delay='10ms', max_queue_size=1000, loss=10, use_htb=True)
+    net.addLink( h3, s1, bw=10, delay='10ms', max_queue_size=1000, loss=10, use_htb=True)
+    net.addLink( h4, s1, bw=10, delay='10ms', max_queue_size=1000, loss=10, use_htb=True)
+
+    info( '*** Starting network\n')
+    net.start()
+
+    info( '*** Running CLI\n' )
+    CLI( net )
+
+    info( '*** Stopping network' )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    emptyNet()
